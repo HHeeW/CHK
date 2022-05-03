26dba0de53cf6ebb2d115d3f375910e6083f753b
134567AA <fnqlqhdms@naver.com>
2022-05-03 14:08:38 +0900

    지우 탑바

--- src/w_comp/TopBar.jsx
+++ src/w_comp/TopBar.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import './topbar.css';
 import {Link} from "react-router-dom";
+import '../B_Event/home.css';
 
 
 export default class TopBar extends Component {
   render() {
     return (
-      
-      <div className="w_container ">
+      <>
+      {/* <div className="w_container ">
         <img src="w_images/logo.png" alt='logo'></img>
         <ul className="w_play">
           <li><i class="fa-solid fa-user-large"></i> MyPage</li>
@@ -19,8 +20,17 @@ export default class TopBar extends Component {
               <span><i class="fa-solid fa-minus"></i></span>
               <li><Link to="/giftshop"> <i class="fa-solid fa-star"></i> Gift-Shop <i class="fa-solid fa-star"></i> </Link></li>
         </ul>
-      </div>
-           
+      </div> */}
+
+      <div className='b_container'>
+        <h1 className='b_title'><Link to="/snack">스토어</Link></h1>
+        <ul className='w_play'>
+        <li><Link to="/snack" ><i class="fa-solid fa-star"></i> Snack </Link></li>
+        <li><Link to="/giftshop" ><i class="fa-solid fa-star"></i>  Gift-Shop</Link></li>
+      </ul> 
+      </div> 
+      
+      </> 
     )
   }
 }
