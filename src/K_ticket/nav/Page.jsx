1c3681b8562d6e53a05f0ce0f23cefbb33125bbc
HeeWon <sun4472@naver.com>
2022-05-12 13:45:47 +0900

    H_예매 연동

--- src/K_ticket/nav/Page.jsx
+++ src/K_ticket/nav/Page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Nav from './Nav';
 import Slide_page2 from './Slide_page2';
-import Main from './Main';
 
 const Page = () => {
+  const name = window.location.hash.substring(1, ).split('%20');
+  console.log(name.join(''));
   return (
     <div>
         <Nav />
